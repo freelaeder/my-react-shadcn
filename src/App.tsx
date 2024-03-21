a0fde84a3030b8f119defb3dd40246e861180ac6
freelaeder <yanglo67@qq.com>
2024-03-21 17:25:59 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {ProfileForm} from "@/pages/LoginForm.tsx";
+
+
+function App() {
+
+  return (
+      <>
+          <div className={'container bg-gray-600 rounded-xl my-4 p-4'}>
+              <ProfileForm />
+          </div>
+      </>
+  )
+}
+
+export default App
